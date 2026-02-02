29568371db5b6fbd565264140ea56d8f056ae4b8
jenisingo2-commits <jenisingo2@gmail.com>
2026-02-02 14:08:55 -0500

    empleado

---
+++ tasks/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render 
+
+# Create your views here.
+def home(request):
+    return render(request,
+                  "home.html")
+
+def signup(request):
+    if request.method == "GET":
+        return render(request,
+                      "signup.html",
+                      {"form": UserCreationForm})
+    else:
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(
+                    username = request.POST["username"],
+                    password= request.POST["password1"]
+                )
+                user.save()
+                return HttpResponse("Usuario creado exitosamente")
+            
+            except:
+                return HttpResponse("Error al crear el usuario")
+
